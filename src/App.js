72a5cd863040c362ace9bfb30420f6a71aa96ce6
sodreamon <60939443+sodreamon@users.noreply.github.com>
2020-03-16 17:16:04 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle, keyframes, css } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 class App extends Component {
   render() {
     return (
       <Container>
+        <GlobalStyle />
         <Button>Hello</Button>
-        <Button danger>Hello</Button>
+        <Button danger rotationTime={2}>
+          Hello
+        </Button>
+        <Anchor as="a" href="https://www.google.com">
+          {" "}
+          Go to google
+        </Anchor>
       </Container>
     );
   }
@@ -32,6 +46,26 @@ const Button = styled.button`
   }
 
   background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
+  ${props => {
+    if (props.danger) {
+      return css`
+        animation: ${Rotation} ${props.rotationTime}s linear infinite;
+      `;
+    }
+  }};
+`;
+
+const Anchor = styled(Button)`
+  text-decoration: none;
+`;
+
+const Rotation = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+  to{
+    transform: rotate(360deg);
+  }
 `;
 
 export default App;
