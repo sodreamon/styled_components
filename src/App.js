18b3a425555568f7caa269222030f3acf53cf87a
sodreamon <60939443+sodreamon@users.noreply.github.com>
2020-03-17 15:15:07 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import styled, { createGlobalStyle, keyframes, css } from "styled-components";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 const GlobalStyle = createGlobalStyle`
   body{
@@ -8,64 +9,38 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-class App extends Component {
-  render() {
-    return (
-      <Container>
-        <GlobalStyle />
-        <Button>Hello</Button>
-        <Button danger rotationTime={2}>
-          Hello
-        </Button>
-        <Anchor as="a" href="https://www.google.com">
-          {" "}
-          Go to google
-        </Anchor>
-      </Container>
-    );
-  }
-}
-
 const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: pink;
 `;
 
+const Card = styled.div`
+  background-color: red;
+`;
 const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:hover {
-    outline: none;
-  }
-
-  background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
-  ${props => {
-    if (props.danger) {
-      return css`
-        animation: ${Rotation} ${props.rotationTime}s linear infinite;
-      `;
-    }
-  }};
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
 `;
 
-const Anchor = styled(Button)`
-  text-decoration: none;
-`;
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Container>
+          <GlobalStyle />
+          <Form />
+        </Container>
+      </ThemeProvider>
+    );
+  }
+}
 
-const Rotation = keyframes`
-  from{
-    transform: rotate(0deg);
-  }
-  to{
-    transform: rotate(360deg);
-  }
-`;
+const Form = () => (
+  <Card>
+    <Button>hello</Button>
+  </Card>
+);
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+  mainColor: "#2ecc71",
+  dangerColor: "#e74c3c",
+  successColor: "#3498db"
+};
+
+export default theme;
